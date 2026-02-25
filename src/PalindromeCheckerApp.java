73f44ca20fb8183bbfa9b5d3d7f7216a0ceacd8a
Palak0905 <pmaheshwari291@gmail.com>
2026-02-25 09:11:20 +0530

    UC2:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,7 +1,25 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        System.out.println("Welcome to Palindrome Checker");
-        System.out.println("Version 1.0");
-        System.out.println("System Initialized successfully");
+        String input = "radar";
+        boolean isPalindrome = true;
+
+        System.out.println("Checking if the word \"" + input + "\" is a palindrome...");
+
+
+        for (int i = 0; i < input.length() / 2; i++) {
+
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+
+        if (isPalindrome) {
+            System.out.println("Result: " + input + " is a palindrome.");
+        } else {
+            System.out.println("Result: " + input + " is not a palindrome.");
+        }
+
     }
 }
