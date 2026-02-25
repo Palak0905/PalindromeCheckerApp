9543398e3e5461a8dc136a891f0cfc5487c47deb
Palak0905 <pmaheshwari291@gmail.com>
2026-02-25 09:53:18 +0530

    UC6:Queue + Stack Based Palindrome Check

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,24 +1,28 @@
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
-    public class PalindromeCheckerApp {
+
+public class PalindromeCheckerApp {
+
     public static void main(String[] args) {
-        String input = "noon";
+
+        String input = "civic";
+
+        Queue<Character> queue = new LinkedList<>();
         Stack<Character> stack = new Stack<>();
 
         for (char c : input.toCharArray()) {
+            queue.add(c);
             stack.push(c);
         }
 
         boolean isPalindrome = true;
 
-        for (char c : input.toCharArray()) {
-            if (c != stack.pop()) {
-                isPalindrome = false;
-                break;
+        while (!queue.isEmpty()) {
+            if (queue.remove() != stack.pop()) {
+
             }
         }
-
-        System.out.println("Input : " + input);
-        System.out.println("Is Palindrome? : " + isPalindrome);
     }
 }
